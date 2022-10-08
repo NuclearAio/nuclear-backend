55e40b2b097f100f7617d3875fc6bf52b90b4f6f
aditya-pushkar <adityapushkar850@gmail.com>
2022-10-09 01:36:14 +0530

    bot vendor with pagination url added

--- bot/urls.py
+++ bot/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
     get_bot_vendors,
+    get_bots_success_rate,
     create_bot_subscription,
     get_bot_subscriptions, 
     delete_bot_subscription
@@ -8,6 +9,7 @@ from .views import (
 
 urlpatterns = [
     path('vendors/', get_bot_vendors, name="get_all_bot_vendors"),
+    path('success_rate/', get_bots_success_rate, name="get_bot_success_rate"),
     path('subscriptions/', get_bot_subscriptions, name="get_bot_subscription"),
     path('create_subscription/', create_bot_subscription, name="create_bot_subscription"),
     path('delete_subscription/<str:bot_subscription_id>/', delete_bot_subscription, name="create_bot_subscription"),
